6e06b1ffd644814fc1eadecddef1eebe4d74e63d
Pranav K <prkrishn@hotmail.com>
2021-08-16 13:12:42 -0700

    Remove some low hanging allocations
    * [x] In the ordinary case, diagnostics aren't produced by builders. The current pattern results in allocating a HashSet
    and an empty array for this case. Removing some of these allocations
    * [x] ViewComponentTagHelperPass does not need to operate on .razor files, but does some unnecessary work. We can avoid it

--- src/Razor/Microsoft.AspNetCore.Mvc.Razor.Extensions/src/ViewComponentTagHelperPass.cs
+++ src/Razor/Microsoft.AspNetCore.Mvc.Razor.Extensions/src/ViewComponentTagHelperPass.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
@@ -15,6 +15,12 @@ namespace Microsoft.AspNetCore.Mvc.Razor.Extensions
 
         protected override void ExecuteCore(RazorCodeDocument codeDocument, DocumentIntermediateNode documentNode)
         {
+            if (FileKinds.IsComponent(codeDocument.GetFileKind()))
+            {
+                // ViewComponent's dont apply to Blazor
+                return;
+            }
+
             var @namespace = documentNode.FindPrimaryNamespace();
             var @class = documentNode.FindPrimaryClass();
             if (@namespace == null || @class == null)
